8026c14c38892b7543c75555b1d1631f7a658ced
fernandobouchet <fernandobouchet@gmail.com>
2022-10-20 01:08:27 -0300

    Fix incorrect value on 'index.js'

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/admin', require('./routes/adminRoute'));
 app.use('/post', require('./routes/postRoute'));
