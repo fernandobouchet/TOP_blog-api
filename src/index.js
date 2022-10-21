a1a48a23991c0efcf7192b6236ed1a3b4fcb46df
fernandobouchet <fernandobouchet@gmail.com>
2022-10-21 20:43:21 -0300

    Import and use 'helmet' and 'compression' to 'index.js'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
+const helmet = require('helmet');
 const connectDB = require('./database/db');
 
 const port = process.env.PORT || 3000;
@@ -9,6 +11,8 @@ const app = express();
 
 connectDB();
 
+app.use(helmet());
+app.use(compression());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
